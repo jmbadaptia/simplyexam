fc40fec214c2d1b283a76d06b1d6997b3aa66ab7
JMB <jmbernal@adaptia.org>
2025-04-14 18:33:33 +0200

    fix: actualiza importaciones en __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,2 @@
 # Este archivo permite importar desde el paquete app
-from .main import create_flask_app as create_app, create_fastapi_app, main
+from .main import create_flask_app as create_app, app, main
